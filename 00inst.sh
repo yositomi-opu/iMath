68053ea51da88c3df21ebb3bedd862ac2de9ccbe
yositomi-opu <yositomi@las.osakafu-u.ac.jp>
2015-12-25 12:48:25 +0900

    Add some useful generic defitions for symbols, exam, etc

--- 00inst.sh
+++ 00inst.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 TARGET=$HOME/texmf/tex/latex/
-install -c -m 644 iMath.sty ${TARGET}/
-install -c -m 644 LinearAlg.sty ${TARGET}/
+install -C -m 644 iMath.sty ${TARGET}/
+install -C -m 644 LinearAlg.sty ${TARGET}/
 for a in *.sty; do ls -l $a ${TARGET}/$a; done
