1c1a647e6837f199a26c48296d3f5bcb8615d8a4
yositomi-opu <yositomi@las.osakafu-u.ac.jp>
2016-02-17 18:18:34 +0900

    add CSVpmatrix and CSVmatrix simplified for pgyouretu / gyouretu of emath

--- 00inst.sh
+++ 00inst.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 TARGET=$HOME/texmf/tex/latex/
 for a in *.sty; do 
-  install -C -m 644 $a ${TARGET}/$a
-  /bin/ls -l ${TARGET}/$a
+  [ -f ${TARGET}/$a ] && mv ${TARGET}/$a ${TARGET}/$a.`date +%s`
+#  install -C -m 644 $a ${TARGET}/$a
+  ln -sf `pwd`/$a ${TARGET}/$a
+#  /bin/ls -l ${TARGET}/$a
 done
